3b125cef5f4dddd59a6564d7a92c68c89776de4d
Kentaro Miki <mikiken.dev@gmail.com>
2024-01-26 01:08:19 +0900

    Update matrix led sample

--- digit_matrix_led_sample/digit_matrix_led_sample.ino
+++ digit_matrix_led_sample/digit_matrix_led_sample.ino
@@ -1,33 +1,83 @@
-#define sin_1 2
-#define sin_2 3
-#define sin_3 4
+#define up_sin_1 2
+#define up_sin_2 3
+#define up_sin_3 4
+#define down_sin_1 12
+#define down_sin_2 13
+#define down_sin_3 14
 #define clock 5
 #define latch 6
 #define enable 7
 
-uint16_t common = 0b0101010101010101;
-uint16_t col_a = 0b0101010101010101;
-uint16_t col_b = 0b0101010101010101;
+uint32_t pattern[32] = {
+    0b00010000100010000100000000011000,
+    0b00010000100010000010001111100000,
+    0b00010011111111100001001000000000,
+    0b00010000100010000000001000000000,
+    0b11111100100010001000001000000000,
+    0b00010011111111100100001111111100,
+    0b00110000001000000010001000010000,
+    0b00111001111111000000001000010000,
+    0b01010101001001000000001000010000,
+    0b01010001001001000000001000010000,
+    0b10010001111111000010111111111110,
+    0b00010001001001000010000000000000,
+    0b00010001001001000100000100010000,
+    0b00010001111111000100001000001000,
+    0b00010000100010001000010000000100,
+    0b00010011000001101000100000000010,
+    0b00000000000000000000000000000000,
+    0b00000000000000000000000000000000,
+    0b01111111110000000111111111111100,
+    0b01000100011111100100000100000100,
+    0b01000100010010000100000100000100,
+    0b01000100010010000100000100000100,
+    0b01111111110010000100000100000100,
+    0b01000100010010000100000100000100,
+    0b01000100010010000111111111111100,
+    0b01000100010010000100000100000100,
+    0b01111111110010000100000100000100,
+    0b00000000000010000100000100000100,
+    0b00000000000010000100000100000100,
+    0b00000000001010000100000100000100,
+    0b00000000000110000111111111111100,
+    0b00000000000000000000000000000000};
 
 void setup() {
-  pinMode(sin_1, OUTPUT);
-  pinMode(sin_2, OUTPUT);
-  pinMode(sin_3, OUTPUT);
-  pinMode(clock, OUTPUT);
-  pinMode(latch, OUTPUT);
-  pinMode(enable, OUTPUT);
+    pinMode(up_sin_1, OUTPUT);
+    pinMode(up_sin_2, OUTPUT);
+    pinMode(up_sin_3, OUTPUT);
+    pinMode(down_sin_1, OUTPUT);
+    pinMode(down_sin_2, OUTPUT);
+    pinMode(down_sin_3, OUTPUT);
+    pinMode(clock, OUTPUT);
+    pinMode(latch, OUTPUT);
+    pinMode(enable, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(enable, LOW);
-  digitalWrite(latch, HIGH);
-  for (int i = 0; i < 16; i++) {
-    digitalWrite(clock, HIGH);
-    digitalWrite(sin_1, (common >> i) & 0b0000000000000001 ? HIGH : LOW);
-    digitalWrite(sin_2, (col_a >> i) & 0b0000000000000001 ? HIGH : LOW);
-    digitalWrite(sin_3, (col_b >> i) & 0b0000000000000001 ? HIGH : LOW);
-    digitalWrite(clock, LOW);
-  }
-  digitalWrite(latch, LOW);
+    digitalWrite(enable, LOW);
+    digitalWrite(latch, HIGH);
+    for (int row = 0; row < 16; row++) {
+        for (int n = 0; n < 16; n++) {
+            // up
+            digitalWrite(up_sin_1, ((0b0000000000000001 << row) >> n) & 0b0000000000000001 ? HIGH : LOW);
+            digitalWrite(up_sin_2, (pattern[row] >> (n + 16)) & 0b0000000000000001 ? HIGH : LOW);
+            digitalWrite(up_sin_3, (pattern[row] >> n) & 0b0000000000000001 ? HIGH : LOW);
+            // down
+            digitalWrite(down_sin_1, ((0b0000000000000001 << row) >> n) & 0b0000000000000001 ? HIGH : LOW);
+            digitalWrite(down_sin_2, (pattern[row + 16] >> (n + 16)) & 0b0000000000000001 ? HIGH : LOW);
+            digitalWrite(down_sin_3, (pattern[row + 16] >> n) & 0b0000000000000001 ? HIGH : LOW);
 
+            digitalWrite(clock, HIGH);
+            digitalWrite(clock, LOW);
+        }
+        delayMicroseconds(800);
+    }
+    digitalWrite(up_sin_1, LOW);
+    digitalWrite(up_sin_2, LOW);
+    digitalWrite(up_sin_3, LOW);
+    digitalWrite(down_sin_1, LOW);
+    digitalWrite(down_sin_2, LOW);
+    digitalWrite(down_sin_3, LOW);
+    digitalWrite(latch, LOW);
 }
